022e4a442cbca1e9c8e20d484919f07da23e8a64
pagal4209 <jaatrudra834@gmail.com>
2025-07-04 11:46:09 +0530

    Update thumbnails.py

--- DnsXMusic/utils/thumbnails.py
+++ DnsXMusic/utils/thumbnails.py
@@ -75,7 +75,7 @@ async def generate_simple_thumb(videoid, filename):
 
     # Center thumbnail (medium size) inside control
     center_thumb = Image.open(f"cache/thumb_{videoid}.jpg").convert("RGBA").resize((180, 150))
-    thumb_cx = 530 - center_thumb.width // 2
+    thumb_cx = 510 - center_thumb.width // 2
     thumb_cy = 360 - center_thumb.height // 2
     background.paste(center_thumb, (thumb_cx, thumb_cy))
 
